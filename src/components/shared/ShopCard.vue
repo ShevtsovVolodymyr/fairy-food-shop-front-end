7d3b7fcd670e86b5b26373acb1f80b740dcff10b
v.shevtsov <versaptr@gmail.com>
2022-07-05 17:52:55 +0300

    updated: corrected card style

--- src/components/shared/ShopCard.vue
+++ src/components/shared/ShopCard.vue
@@ -52,6 +52,7 @@ export default {
   flex-direction: column;
   margin: 0;
   min-width: 320px;
+  max-width: 320px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   border-radius: 6px;
   padding: 32px 16px 16px 16px;
